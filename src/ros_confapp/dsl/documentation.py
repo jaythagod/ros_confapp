f1cf81bdf9dd894c8567ed55c2aade8f86df0024
jtg <jude.gyimah87@gmail.com>
2021-07-25 23:50:51 +0200

    markdown bug fixed in docs

--- src/ros_confapp/dsl/documentation.py
+++ src/ros_confapp/dsl/documentation.py
@@ -1,24 +1,22 @@
+#!/usr/bin/env python
 import os
 import markdown
-#from markdown.__meta__ import __version_info__
-#from html import parser # unused but force pyinstaller to load 
 from bs4 import BeautifulSoup
 
 class Documentation:
     def __init__(self):
-        self.docPath = os.path.dirname(os.path.abspath('docs.md'))
+        self.docPath = os.path.dirname(os.path.abspath('../docs/docs.md'))
 
 
     def subsetManual(self, section):
         docuFile = os.path.join(self.docPath, 'docs.md')
-        print(docuFile)
         html = markdown.markdown(open(docuFile).read())
-        extractedSection = "".join(BeautifulSoup(html, features="html.parser").find(class_=section))
+        extractedSection = "".join(BeautifulSoup(html, 'html5lib').find(class_=section))
         print(extractedSection)
 
     def fullManual(self):
         docuFile = os.path.join(self.docPath, 'docs.md')
         html = markdown.markdown(open(docuFile).read())
-        extractedDocText = "".join(BeautifulSoup(html, features="html.parser").findAll(text=True))
+        extractedDocText = "".join(BeautifulSoup(html, 'html5lib').findAll(text=True))
         print(extractedDocText)
 
